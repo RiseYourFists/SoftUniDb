2d1a45bb67dd68d00fcbae3336d83797729101b8
Emanuel Shavepeyer <daru12378@gmail.com>
2022-08-20 20:06:37 +0300

    Problem solving

---
+++ SoftUniAdvancedProgramming/SoftuniAdvanced/SimpleCalculator/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SimpleCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = 0;
+            var expression = Console.ReadLine().Split().Reverse();
+            var stack = new Stack<string>();
+
+            foreach (var item in expression)
+            {
+                stack.Push(item);
+            }
+
+            result = int.Parse(stack.Pop());
+
+            while (stack.Count > 0)
+            {
+                var symbol = stack.Pop();
+                var num = int.Parse(stack.Pop().ToString());
+
+                if(symbol == "+")
+                {
+                    result += num;
+                    continue;
+                }
+
+                result -= num;
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
