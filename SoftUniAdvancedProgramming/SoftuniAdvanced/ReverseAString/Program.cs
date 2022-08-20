17d2e7bcf690281faa06c1a9ffdc4c17836b25cc
Emanuel Shavepeyer <daru12378@gmail.com>
2022-08-20 17:07:47 +0300

    First Commit

---
+++ SoftUniAdvancedProgramming/SoftuniAdvanced/ReverseAString/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ReverseString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+
+            var stack = new Stack<char>();
+            foreach (var item in input)
+            {
+                stack.Push(item);
+            }
+
+            var result = new StringBuilder();
+
+            while (stack.Count > 0)
+            {
+                result.Append(stack.Pop());
+            }
+
+            Console.WriteLine(result.ToString());
+        }
+    }
+}
