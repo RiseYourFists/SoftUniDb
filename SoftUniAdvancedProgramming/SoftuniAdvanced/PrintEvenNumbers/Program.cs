0f1da7c4d1cb9111e7c5a001e720baca8110f76c
Emanuel Shavepeyer <daru12378@gmail.com>
2022-08-21 18:00:35 +0300

    Problem solving

---
+++ SoftUniAdvancedProgramming/SoftuniAdvanced/PrintEvenNumbers/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PrintEvenNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var intArray = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var numberQueue = new Queue<int>();
+
+            foreach (var item in intArray)
+            {
+                numberQueue.Enqueue(item);
+            }
+
+            var result = new List<int>();
+
+            while (numberQueue.Count > 0)
+            {
+                var currNum = numberQueue.Dequeue();
+
+                if (currNum % 2 == 0)
+                {
+                    result.Add(currNum);
+                }
+            }
+
+            Console.WriteLine(string.Join(", ", result));
+        }
+    }
+}
