a2efe4cb96625e1bd5d95f46106de43178038a6f
Emanuel Shavepeyer <daru12378@gmail.com>
2022-08-21 19:44:38 +0300

    Problem solving

---
+++ SoftUniAdvancedProgramming/SoftuniAdvanced/HotPotatoAlternativeSolution/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HotPotatoAlternativeSolution
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var players = new Queue<string>(Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries));
+            var turns = int.Parse(Console.ReadLine());
+
+            while (players.Count > 1)
+            {
+                for (int i = 0; i < turns - 1; i++)
+                {
+                    players.Enqueue(players.Dequeue());
+                }
+
+                Console.WriteLine($"Removed {players.Dequeue()}");
+            }
+
+            Console.WriteLine($"Last is {players.Dequeue()}");
+        }
+    }
+}
