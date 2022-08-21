ce5369339057aa2921090b6c50fe222e2cb8574d
Emanuel Shavepeyer <daru12378@gmail.com>
2022-08-21 17:45:17 +0300

    Problem solving

---
+++ SoftUniAdvancedProgramming/SoftuniAdvanced/MatchingBrackets/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MatchingBrackets
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var expression = Console.ReadLine();
+            var startingBracket = new Stack<int>();
+
+            for (int i = 0; i < expression.Length; i++)
+            {
+                if(expression[i] == '(')
+                {
+                    startingBracket.Push(i);
+                }
+
+                if(expression[i] == ')')
+                {
+                    var startPos = startingBracket.Pop();
+                    var len =  i + 1 - startPos;
+                    Console.WriteLine(expression.Substring(startPos, len));
+                }
+            }
+        }
+    }
+}
