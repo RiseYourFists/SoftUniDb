7da3cec70ea29130a2608ab4ea2f8284e786de2c
Emanuel Shavepeyer <daru12378@gmail.com>
2022-08-21 18:18:30 +0300

    Problem solving

---
+++ SoftUniAdvancedProgramming/SoftuniAdvanced/SuperMarket/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SuperMarket
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var customerQueue = new Queue<string>();
+
+            while (true)
+            {
+                var input = Console.ReadLine();
+
+                if(input == "End")
+                {
+                    break;
+                }
+
+                if(input == "Paid")
+                {
+                    PrintAndClearCustomerList(customerQueue);
+                    continue;
+                }
+
+                customerQueue.Enqueue(input);
+            }
+
+            Console.WriteLine($"{customerQueue.Count} people remaining.");
+        }
+
+        private static void PrintAndClearCustomerList(Queue<string> customerQueue)
+        {
+            while (customerQueue.Count > 0)
+            {
+                Console.WriteLine(customerQueue.Dequeue());
+            }
+        }
+    }
+}
