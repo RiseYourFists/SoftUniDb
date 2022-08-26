345b7f9250fceda263d485800d828dfbfa1ff6fd
Emanuel Shavepeyer <daru12378@gmail.com>
2022-08-26 17:34:33 +0300

    Problem solving

---
+++ SoftUniAdvancedProgramming/SoftuniAdvanced/FastFood/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FastFood
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var initialFoodQuantity = int.Parse(Console.ReadLine());
+
+            var orders = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            var orderQueue = new Queue<int>(orders);
+            var biggest = int.MinValue;
+            while (orderQueue.Count > 0)
+            {
+                if(biggest < orderQueue.Peek())
+                {
+                    biggest = orderQueue.Peek();
+                }
+
+                if(orderQueue.Peek() > initialFoodQuantity)
+                {
+                    break;
+                }
+
+                initialFoodQuantity -= orderQueue.Dequeue();
+            }
+
+            Console.WriteLine(biggest);
+
+            if(orderQueue.Count > 0)
+            {
+                Console.Write("Orders left: ");
+
+                while (orderQueue.Count > 0)
+                {
+                    Console.Write(orderQueue.Dequeue() + " ");
+                }
+                return;
+            }
+
+            Console.WriteLine("Orders complete");
+        }
+    }
+}
