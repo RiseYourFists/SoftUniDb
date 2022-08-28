b539e3bf3d639b4f57e3c723c9ed31d02cfea2d2
Emanuel Shavepeyer <daru12378@gmail.com>
2022-08-28 22:43:03 +0300

    Problem solving

---
+++ SoftUniAdvancedProgramming/SoftuniAdvanced/BalancedParentheses/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BalancedParentheses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var charStack = new Stack<char>();
+            var input = Console.ReadLine();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                var ch = input[i];
+
+                if (ch == '{' || ch == '(' || ch == '[')
+                {
+                    charStack.Push(ch);
+                    continue;
+                }
+
+                if (charStack.Count == 0)
+                {
+                    Console.WriteLine("NO");
+                    return;
+                }
+                char opening = ' ';
+
+                switch (ch)
+                {
+                    case ')':
+                        opening = '(';
+                        break;
+                    case ']':
+                        opening = '[';
+                        break;
+                    case '}':
+                        opening = '{';
+                        break;
+                }
+
+                if (opening == charStack.Pop())
+                {
+                    continue;
+                }
+
+                Console.WriteLine("NO");
+                return;
+            }
+
+            Console.WriteLine("YES");
+        }
+    }
+}
