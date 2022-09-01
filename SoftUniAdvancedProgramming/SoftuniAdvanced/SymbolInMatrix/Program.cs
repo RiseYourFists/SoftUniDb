d1fa68fb90a5defaaaf5d999e11eaf6c55d946cd
Emanuel Shavepeyer <daru12378@gmail.com>
2022-09-01 20:49:25 +0300

    Problem solving

---
+++ SoftUniAdvancedProgramming/SoftuniAdvanced/SymbolInMatrix/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+
+namespace SymbolInMatrix
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var size = int.Parse(Console.ReadLine());
+
+            var matrix = new char[size, size];
+
+            for (int i = 0; i < size; i++)
+            {
+                var input = Console.ReadLine();
+
+                for (int j = 0; j < size; j++)
+                {
+                    matrix[i, j] = input[j];
+                }
+            }
+
+            var symbol = Console.ReadLine();
+
+            for (int row = 0; row < size; row++)
+            {
+                for (int col = 0; col < size; col++)
+                {
+                    var ch = symbol.ToCharArray();
+                    if (matrix[row,col] == ch[0])
+                    {
+                        Console.WriteLine($"({row}, {col})");
+                        return;
+                    }
+                }
+            }
+            
+            Console.WriteLine($"{symbol} does not occur in the matrix");
+        }
+    }
+}
