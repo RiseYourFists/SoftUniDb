743e628075edfbd134690d1e8896d0c97157b303
Emanuel Shavepeyer <daru12378@gmail.com>
2022-09-01 20:30:32 +0300

    Problem solving

---
+++ SoftUniAdvancedProgramming/SoftuniAdvanced/SumOfPrimaryDiagonal/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace SumOfPrimaryDiagonal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var size = int.Parse(Console.ReadLine());
+
+            var matrix = new int[size, size];
+
+            for (int i = 0; i < size; i++)
+            {
+                var input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+                for (int j = 0; j < size; j++)
+                {
+                    matrix[i, j] = input[j];
+                }
+            }
+
+            var result = 0;
+
+            for (int i = 0; i < size; i++)
+            {
+                result += matrix[i, i];
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
