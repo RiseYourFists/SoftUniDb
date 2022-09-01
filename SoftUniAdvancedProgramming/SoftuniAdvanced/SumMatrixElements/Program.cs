879b3666676a426fd0c56baa387d662daaf61fed
Emanuel Shavepeyer <daru12378@gmail.com>
2022-09-01 20:02:32 +0300

    Problem solving

---
+++ SoftUniAdvancedProgramming/SoftuniAdvanced/SumMatrixElements/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace SumMatrixElements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var size = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+            var matrix = new int[size[0], size[1]];
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                var input = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    matrix[i, j] = input[j];
+                }
+            }
+
+            var result = 0;
+
+            foreach (var item in matrix)
+            {
+                result += item;
+            }
+
+            Console.WriteLine($"{size[0]}\n{size[1]}\n{result}");
+        }
+    }
+}
