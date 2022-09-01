2fd2df17a6c44b4cf6e8858a63133191193eac34
Emanuel Shavepeyer <daru12378@gmail.com>
2022-09-01 20:17:52 +0300

    Problem solving

---
+++ SoftUniAdvancedProgramming/SoftuniAdvanced/SumOfMatrixColumns/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+namespace SumOfMatrixColumns
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var size = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+            var matrix = new int[size[0], size[1]];
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                var input = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    matrix[i, j] = input[j];
+                }
+            }
+
+            for (int i = 0; i < size[1]; i++)
+            {
+                var result = 0;
+
+                for (int j = 0; j < size[0]; j++)
+                {
+                    result += matrix[j, i];
+                }
+
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
