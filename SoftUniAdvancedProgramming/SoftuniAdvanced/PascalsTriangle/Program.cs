7ecd7e5a5e008bc5ef22eb5771469ba2a3702f1b
Emanuel Shavepeyer <daru12378@gmail.com>
2022-09-08 21:16:40 +0300

    Problem solving

---
+++ SoftUniAdvancedProgramming/SoftuniAdvanced/PascalsTriangle/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Text;
+
+namespace PascalTriangleDemo
+{
+    class Program
+    {
+        public static void Main()
+        {
+            int rows = int.Parse(Console.ReadLine());
+            var jaggedArray = new long[rows][];
+            long val = 1;
+
+            for (int i = 0; i < rows; i++)
+            {
+                var tempArray = new long[i + 1];
+
+                for (int j = 0; j <= i; j++)
+                {
+                    if (j == 0 || i == 0)
+                    {
+                        val = 1;
+                    }
+                    else
+                    {
+                        val = val * (i - j + 1) / j;
+                    }
+
+                    tempArray[j] = val;
+                }
+
+                jaggedArray[i] = tempArray;
+            }
+
+            var result = new StringBuilder();
+
+            for (int i = 0; i < jaggedArray.GetLength(0); i++)
+            {
+                for (int j = 0; j < jaggedArray[i].Length; j++)
+                {
+                    result.Append(jaggedArray[i][j] + " ");
+                }
+                result.Append(Environment.NewLine);
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
