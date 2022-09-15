c79961663cd30db93b353cdd6899797f4e90df16
Emanuel Shavepeyer <daru12378@gmail.com>
2022-09-15 22:42:54 +0300

    Problem solving

---
+++ SoftUniAdvancedProgramming/SoftuniAdvanced/Largest3Numbers/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace Largest3Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+            var result = input.OrderByDescending(x => x).Take(3).ToArray();
+
+            Console.WriteLine(String.Join(' ', result));
+        }
+    }
+}
