fb6f1a6d285a0ce518b994f4ebca7c272d303972
Emanuel Shavepeyer <daru12378@gmail.com>
2022-09-15 22:06:36 +0300

    Problem solving

---
+++ SoftUniAdvancedProgramming/SoftuniAdvanced/SameValuesInArray/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SameValuesInArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(double.Parse).ToArray();
+
+            var occurenceDictionary = new Dictionary<double, int>();
+
+            foreach (var item in input)
+            {
+                if(occurenceDictionary.ContainsKey(item))
+                {
+                    occurenceDictionary[item]++;
+                    continue;
+                }
+
+                occurenceDictionary.Add(item, 1);
+            }
+
+            foreach (var item in occurenceDictionary)
+            {
+                Console.WriteLine($"{item.Key} - {item.Value} times");
+            }
+        }
+    }
+}
