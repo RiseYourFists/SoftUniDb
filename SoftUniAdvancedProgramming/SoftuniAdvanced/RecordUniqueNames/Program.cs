dcdf3ac8f4c1ebb829ccdfd894640e7cacf3b4b1
Emanuel Shavepeyer <daru12378@gmail.com>
2022-09-21 21:04:51 +0300

    Problem solving

---
+++ SoftUniAdvancedProgramming/SoftuniAdvanced/RecordUniqueNames/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RecordUniqueNames
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputs = int.Parse(Console.ReadLine());
+
+            var uniqueNames = new HashSet<string>();
+
+            for (int i = 0; i < inputs; i++)
+            {
+                uniqueNames.Add(Console.ReadLine());
+            }
+
+            Console.WriteLine(string.Join(Environment.NewLine, uniqueNames));
+        }
+    }
+}
