28ffdc1e061c49c01698eb44bde57636fc5cb20d
Emanuel Shavepeyer <daru12378@gmail.com>
2022-09-21 21:18:33 +0300

    Problem solving

---
+++ SoftUniAdvancedProgramming/SoftuniAdvanced/ParkingLot/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ParkingLot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var parkingLot = new HashSet<string>();
+
+            while (true)
+            {
+                var input = Console.ReadLine();
+
+                if (input == "END")
+                {
+                    break;
+                }
+
+                var tokens = input.Split(", ");
+
+                if (tokens[0] == "IN")
+                {
+                    parkingLot.Add(tokens[1]);
+                    continue;
+                }
+
+                parkingLot.Remove(tokens[1]);
+            }
+
+            if(parkingLot.Count == 0)
+            {
+                Console.WriteLine("Parking Lot is Empty");
+                return;
+            }
+
+            Console.WriteLine(string.Join(Environment.NewLine, parkingLot));
+        }
+    }
+}
