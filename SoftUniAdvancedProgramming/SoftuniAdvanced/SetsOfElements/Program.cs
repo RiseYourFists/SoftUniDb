97b3477a57b0b3d06291f6b0950d59510cbaddb9
Emanuel Shavepeyer <daru12378@gmail.com>
2022-09-22 16:24:36 +0300

    Problem solving

---
+++ SoftUniAdvancedProgramming/SoftuniAdvanced/SetsOfElements/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace SetsOfElements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var setSizes = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+            var setOne = new HashSet<string>();
+            var setTwo = new HashSet<string>();
+
+            for (int i = 0; i < setSizes[0]; i++)
+            {
+                setOne.Add(Console.ReadLine());
+            }
+
+            for (int i = 0; i < setSizes[1]; i++)
+            {
+                setTwo.Add(Console.ReadLine());
+            }
+
+            string[] result;
+
+            if (setSizes[0] >= setSizes[1])
+            {
+                result = setOne.Where(x => setTwo.Contains(x)).ToArray();
+            }
+            else
+            {
+                result = setTwo.Where(x => setOne.Contains(x)).ToArray();
+            }
+
+            Console.WriteLine(string.Join(' ', result));
+        }
+    }
+}
