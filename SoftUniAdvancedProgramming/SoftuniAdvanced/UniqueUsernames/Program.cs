3e331b408a567ff7c3f23ce13d1e404120a15ba6
Emanuel Shavepeyer <daru12378@gmail.com>
2022-09-22 15:49:19 +0300

    Problem solving

---
+++ SoftUniAdvancedProgramming/SoftuniAdvanced/UniqueUsernames/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace UniqueUsernames
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputs = int.Parse(Console.ReadLine());
+            var uniqueUsers = new HashSet<string>();
+
+            for (int i = 0; i < inputs; i++)
+            {
+                uniqueUsers.Add(Console.ReadLine());
+            }
+
+            Console.WriteLine(string.Join(Environment.NewLine, uniqueUsers));
+        }
+    }
+}
