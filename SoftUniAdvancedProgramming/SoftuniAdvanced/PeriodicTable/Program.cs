484cbe180205e1ad5181fa2532fb8cf8e18c5ff9
Emanuel Shavepeyer <daru12378@gmail.com>
2022-09-22 17:41:03 +0300

    Problem solving

---
+++ SoftUniAdvancedProgramming/SoftuniAdvanced/PeriodicTable/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PeriodicTable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputs = int.Parse(Console.ReadLine());
+            var periodicTable = new SortedSet<string>();
+
+            for (int i = 0; i < inputs; i++)
+            {
+                var input = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                for (int j = 0; j < input.Length; j++)
+                {
+                    periodicTable.Add(input[j]);
+                }
+            }
+
+            Console.WriteLine(string.Join(' ', periodicTable));
+        }
+    }
+}
