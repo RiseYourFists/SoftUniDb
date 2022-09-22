859a54d4280abf946ecf42d463326b203bf714e8
Emanuel Shavepeyer <daru12378@gmail.com>
2022-09-22 18:03:20 +0300

    Problem solving

---
+++ SoftUniAdvancedProgramming/SoftuniAdvanced/CountSymbols/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CountSymbols
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var symbols = Console.ReadLine().ToCharArray();
+
+            var symbolOccurances = new SortedDictionary<char, int>();
+
+            for (int i = 0; i < symbols.Length; i++)
+            {
+                if (symbolOccurances.ContainsKey(symbols[i]))
+                {
+                    symbolOccurances[symbols[i]]++;
+                    continue;
+                }
+
+                symbolOccurances.Add(symbols[i], 1);
+            }
+
+            foreach (var item in symbolOccurances)
+            {
+                Console.WriteLine($"{item.Key}: {item.Value} time/s");
+            }
+        }
+    }
+}
