d88eb099395e6656803cb3e19f9e1b5c73a112d5
Emanuel Shavepeyer <daru12378@gmail.com>
2022-09-22 17:49:41 +0300

    Problem solving

---
+++ SoftUniAdvancedProgramming/SoftuniAdvanced/EvenTimes/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EvenTimes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputs = int.Parse(Console.ReadLine());
+
+            var numbers = new Dictionary<string, int>();
+
+            for (int i = 0; i < inputs; i++)
+            {
+                var newNum = Console.ReadLine();
+
+                if(numbers.ContainsKey(newNum))
+                {
+                    numbers[newNum]++;
+                    continue;
+                }
+
+                numbers.Add(newNum, 1);
+            }
+
+            var result = numbers.First(x => x.Value % 2 == 0);
+            Console.WriteLine(result.Key);
+        }
+    }
+}
