26a784f1b334811295c4738f4cadc9c402a1df1e
Emanuel Shavepeyer <daru12378@gmail.com>
2022-09-26 00:37:29 +0300

    Problem solving

---
+++ SoftUniAdvancedProgramming/SoftuniAdvanced/OddLines/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+
+namespace OddLines
+{
+    internal class OddLines
+    {
+        static void Main(string[] args)
+        {
+            var fileInput = @"../../../input.txt";
+            var fileOutput = @"../../../output.txt";
+
+            ExtractOddLines(fileInput, fileOutput);
+        }
+
+        private static void ExtractOddLines(string fileInput, string fileOutput)
+        {
+            using (var fileStream = new StreamReader(fileInput))
+            {
+                var text = fileStream.ReadLine();
+                int line = 1;
+
+                using (var writer = new StreamWriter(fileOutput))
+                {
+                    while (text != null)
+                    {
+                        if (line % 2 == 1)
+                        {
+                            writer.WriteLine($"{line}. {text}");
+                        }
+                        line++;
+
+                        text = fileStream.ReadLine();
+                    }
+                }
+            }
+        }
+    }
+}
