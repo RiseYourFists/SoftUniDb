ec85abe69b417997eccc5432ce2c89240b460712
Emanuel Shavepeyer <daru12378@gmail.com>
2022-09-26 17:58:57 +0300

    Problem solving

---
+++ SoftUniAdvancedProgramming/SoftuniAdvanced/MergeFiles/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace MergeFiles
+{
+    using System;
+    using System.IO;
+    public class MergeFiles
+    {
+        static void Main(string[] args)
+        {
+            var firstInputFilePath = @"..\..\..\Files\input1.txt";
+            var secondInputFilePath = @"..\..\..\Files\input2.txt";
+            var outputFilePath = @"..\..\..\Files\output.txt";
+
+            MergeTextFiles(firstInputFilePath, secondInputFilePath, outputFilePath);
+        }
+
+        public static void MergeTextFiles(string firstInputFilePath, string secondInputFilePath, string outputFilePath)
+        {
+            using(var file1Reader = new StreamReader(firstInputFilePath))
+            {
+                using(var file2Reader = new StreamReader(secondInputFilePath))
+                {
+                    using( var outputFile = new StreamWriter(outputFilePath))
+                    {
+                        while (true)
+                        {
+                            var line1 = file1Reader.ReadLine();
+                            var line2 = file2Reader.ReadLine();
+
+                            if (line2 == null && line1 == null) break;
+
+                            outputFile.WriteLine(line1);
+                            outputFile.WriteLine(line2);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
