acfc1b384f688c6a5cbe3f3403e180b125a9167c
Emanuel Shavepeyer <daru12378@gmail.com>
2022-09-26 16:52:02 +0300

    Problem solving

---
+++ SoftUniAdvancedProgramming/SoftuniAdvanced/LineNumbers/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace LineNumbers
+{
+    using System;
+    using System.IO;
+
+    public class LineNumbers
+    {
+        static void Main()
+        {
+            string inputFilePath = @"..\..\..\Files\input.txt";
+            string outputFilePath = @"..\..\..\Files\output.txt";
+
+            RewriteFileWithLineNumbers(inputFilePath, outputFilePath);
+        }
+
+        public static void RewriteFileWithLineNumbers(string inputFilePath, string outputFilePath)
+        {
+            using (var streamReader = new StreamReader(inputFilePath))
+            {
+                var line = streamReader.ReadLine();
+                var counter = 1;
+
+                using (var streamWriter = new StreamWriter(outputFilePath))
+                {
+                    while (line != null)
+                    {
+                        streamWriter.WriteLine($"{counter}. {line}");
+                        counter++;
+                        line = streamReader.ReadLine();
+                    }
+                }
+            }
+        }
+    }
+
+}
