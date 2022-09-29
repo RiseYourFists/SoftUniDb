5d66d96833da6e8b9d2ed4db2ac6e0ce932db738
Emanuel Shavepeyer <daru12378@gmail.com>
2022-09-29 14:40:00 +0300

    Problem solving

---
+++ SoftUniAdvancedProgramming/SoftuniAdvanced/LineNumbersExercise/Program.cs
@@ -0,0 +1,64 @@
+﻿namespace LineNumbers
+{
+    using System;
+    using System.IO;
+    using System.Linq;
+
+    public class LineNumbers
+    {
+        static void Main(string[] args)
+        {
+            string inputPath = @"..\..\..\text.txt";
+            string outputPath = @"..\..\..\output.txt";
+
+            ProcessLines(inputPath, outputPath);
+        }
+
+        public static void ProcessLines(string inputFilePath, string outputFilePath)
+        {
+            using (var reader = new StreamReader(inputFilePath))
+            using (var writer = new StreamWriter(outputFilePath))
+            {
+                string line;
+                var counter = 1;
+                while((line = reader.ReadLine()) != null)
+                {
+                    var letters = GetLetterCount(line);
+                    var punctuation = GetPunctuationCount(line);
+                    writer.WriteLine($"Line {counter}: {line} ({letters})({punctuation})");
+                    counter++;
+                }
+            }
+        }
+
+        private static int GetPunctuationCount(string line)
+        {
+            var result = 0;
+            var charArray = line.ToCharArray();
+            var punctuation = new char[] { '-', ',', '.', '!', '?', '\'' };
+            foreach (var ch in charArray)
+            {
+                if (punctuation.Contains(ch))
+                {
+                    result++;
+                }
+            }
+            return result;
+        }
+
+        private static object GetLetterCount(string line)
+        {
+            
+            var result = 0;
+            var charArray = line.ToCharArray();
+            foreach (var ch in charArray)
+            {
+                if (char.IsLetter(ch))
+                {
+                    result++;
+                }
+            }
+            return result;
+        }
+    }
+}
