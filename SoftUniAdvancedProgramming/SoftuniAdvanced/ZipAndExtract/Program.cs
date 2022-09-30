305bc1012f37b33f4c997983e11a65d5691be803
Emanuel Shavepeyer <daru12378@gmail.com>
2022-09-30 18:13:14 +0300

    Problem solving

---
+++ SoftUniAdvancedProgramming/SoftuniAdvanced/ZipAndExtract/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace ZipAndExtract
+{
+    using System;
+    using System.IO;
+    using System.IO.Compression;
+
+    public class ZipAndExtract
+    {
+        static void Main(string[] args)
+        {
+            string inputFile = @"..\..\..\copyMe.png";
+            string zipArchiveFile = @"..\..\..\archive.zip";
+            string extractedFile = @"..\..\..\extracted.png";
+
+            ZipFileToArchive(inputFile, zipArchiveFile);
+
+            var fileNameOnly = Path.GetFileName(inputFile);
+            ExtractFileFromArchive(zipArchiveFile, fileNameOnly, extractedFile);
+        }
+
+        public static void ZipFileToArchive(string inputFilePath, string zipArchiveFilePath)
+        {
+            using (var zipfile = ZipFile.Open(zipArchiveFilePath, ZipArchiveMode.Create))
+            {
+                zipfile.CreateEntryFromFile(inputFilePath, "copyMe.png");
+            }
+        }
+
+        public static void ExtractFileFromArchive(string zipArchiveFilePath, string fileName, string outputFilePath)
+        {
+            using(var zipfile = ZipFile.OpenRead(zipArchiveFilePath))
+            {
+                var currZip = zipfile.GetEntry(fileName);
+                currZip.ExtractToFile(outputFilePath);
+            }
+        }
+    }
+}
