265380cf6689b36f9e72e3aac3d64922f14928a3
Emanuel Shavepeyer <daru12378@gmail.com>
2022-10-01 18:10:20 +0300

    Problem solving

---
+++ SoftUniAdvancedProgramming/SoftuniAdvanced/CountUppercaseWords/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace CountUppercaseWords
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<string, bool> isFirstUpper = x => char.IsUpper(x[0]);
+            var input = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            var result = input.Where(isFirstUpper).ToList();
+            result.ForEach(x => Console.WriteLine(x));
+        }
+
+    }
+}
