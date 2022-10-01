7772879a3ff950a98d1cc9146702095f641e0ec7
Emanuel Shavepeyer <daru12378@gmail.com>
2022-10-01 18:53:28 +0300

    Problem solving

---
+++ SoftUniAdvancedProgramming/SoftuniAdvanced/AddVAT/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace AddVAT
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var VAT = 0.2;
+            Func<double, double> addVAT = x => x + x * VAT;
+            var input = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(x => addVAT(double.Parse(x)))
+                .ToList();
+
+            input.ForEach(x => Console.WriteLine($"{x:f2}"));
+        }
+    }
+}
