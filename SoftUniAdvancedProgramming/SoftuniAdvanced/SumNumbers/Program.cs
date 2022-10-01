64ec2d6f5ca476f44903795f45929b18dd402ea7
Emanuel Shavepeyer <daru12378@gmail.com>
2022-10-01 16:17:53 +0300

    problem solving

---
+++ SoftUniAdvancedProgramming/SoftuniAdvanced/SumNumbers/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace SumNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+            Console.WriteLine(input.Count);
+            Console.WriteLine(input.Sum());
+        }
+    }
+}
