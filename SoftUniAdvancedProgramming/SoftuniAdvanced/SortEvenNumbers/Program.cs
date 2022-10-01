bbfe8d894a7fb56a09e0498d9816cccf9fbfc209
Emanuel Shavepeyer <daru12378@gmail.com>
2022-10-01 15:41:05 +0300

    Problem solving

---
+++ SoftUniAdvancedProgramming/SoftuniAdvanced/SortEvenNumbers/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace SortEvenNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            var result = input.Where(x => x % 2 == 0).OrderBy(x => x).ToArray();
+            Console.WriteLine(string.Join(", ", result));
+        }
+    }
+}
