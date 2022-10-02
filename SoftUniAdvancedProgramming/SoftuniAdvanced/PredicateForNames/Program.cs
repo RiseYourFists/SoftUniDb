2db8a2f47b2934a2fe2b2d1ee0a61fcaee32fe8e
Emanuel Shavepeyer <daru12378@gmail.com>
2022-10-03 00:57:25 +0300

    Problem solving

---
+++ SoftUniAdvancedProgramming/SoftuniAdvanced/PredicateForNames/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace PredicateForNames
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            Predicate<string> predicate = (x => x.Length <= n);
+            Func<string, bool> validate = (x => predicate(x));
+            var input = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).ToList();
+            var result = input.Where(validate).ToList();
+            result.ForEach(x => Console.WriteLine(x));
+        }
+    }
+}
