97f7becb9172238d79bb15a2adb05cbd8a9dd5ba
Emanuel Shavepeyer <daru12378@gmail.com>
2022-10-02 23:08:17 +0300

    Problem solving

---
+++ SoftUniAdvancedProgramming/SoftuniAdvanced/CustomMinFunction/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace CustomMinFunction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<int[], int> minFunc = GetMin;
+            var input = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            Console.WriteLine(minFunc(input));
+        }
+
+        private static int GetMin(int[] collection)
+        {
+            var min = int.MaxValue;
+
+            foreach (var number in collection)
+            {
+                if(number < min)
+                {
+                    min = number;
+                }
+            }
+
+            return min;
+        }
+    }
+}
