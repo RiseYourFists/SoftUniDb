ed49dee21f5e1ba51690bca756470660ce852f79
Emanuel Shavepeyer <daru12378@gmail.com>
2022-10-02 22:44:31 +0300

    Problem solving

---
+++ SoftUniAdvancedProgramming/SoftuniAdvanced/KnightsOfHonor/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace KnightsOfHonor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Action<string> AddSir = (x => Console.WriteLine($"Sir {x}"));
+            var input = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).ToList();
+            input.ForEach(AddSir);
+        }
+    }
+}
