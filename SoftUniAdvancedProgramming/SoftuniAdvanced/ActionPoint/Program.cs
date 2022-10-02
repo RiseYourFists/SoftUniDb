1f8990c2685821153a39010136e484de64014c1d
Emanuel Shavepeyer <daru12378@gmail.com>
2022-10-02 22:37:27 +0300

    Problem solving

---
+++ SoftUniAdvancedProgramming/SoftuniAdvanced/ActionPoint/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace ActionPoint
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Action<string> print = Console.WriteLine;
+            var input = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            input.ToList().ForEach(print);
+        }
+    }
+}
