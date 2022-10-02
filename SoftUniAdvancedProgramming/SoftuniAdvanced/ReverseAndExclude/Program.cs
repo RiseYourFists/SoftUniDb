98ce2f5518cd65246b9537877de9ed51319dc6ff
Emanuel Shavepeyer <daru12378@gmail.com>
2022-10-03 00:42:45 +0300

    Problem solving

---
+++ SoftUniAdvancedProgramming/SoftuniAdvanced/ReverseAndExclude/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace ReverseAndExclude
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+            input.Reverse();
+            var n = int.Parse(Console.ReadLine());
+            Func<int, bool> validate = (x => x % n != 0);
+            var result = input.Where(validate).ToList();
+            result.ForEach(x => Console.Write($"{x} "));
+
+        }
+    }
+}
