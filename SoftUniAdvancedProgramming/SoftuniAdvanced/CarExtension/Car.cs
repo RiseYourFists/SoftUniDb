93c5d6fdfab49c8d72b61ab77455e0023c05aed4
Emanuel Shavepeyer <daru12378@gmail.com>
2022-10-06 17:39:49 +0300

    Problem solving

---
+++ SoftUniAdvancedProgramming/SoftuniAdvanced/CarExtension/Car.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace CarManufacturer
+{
+
+    public class Car
+    {
+
+        public string Make { get { return make; } set { make = value; } }
+        public string Model { get { return model; } set { model = value; } }
+        public int Year { get { return year; } set { year = value; } }
+        public double FuelQuantity { get { return fuelQuantity; } set { fuelQuantity = value; } }
+        public double FuelConsumption { get { return fuelConsumption; } set { fuelConsumption = value; } }
+
+        private string make;
+        private string model;
+        private int year;
+        private double fuelQuantity;
+        private double fuelConsumption;
+
+        public void Drive(double distance)
+        {
+            var burnedFuel = fuelConsumption * distance;
+
+            if (!(fuelQuantity - burnedFuel > 0))
+            {
+                Console.WriteLine("Not enough fuel to perform this trip!");
+                return;
+            }
+
+            fuelQuantity -= burnedFuel;
+        }
+
+        public string WhoAmI()
+        {
+            var info = $"Make: {this.Make}\nModel: {this.Model}\nYear: {this.Year}\nFuel: {this.FuelQuantity:F2}";
+            return info;
+        }
+    }
+}
