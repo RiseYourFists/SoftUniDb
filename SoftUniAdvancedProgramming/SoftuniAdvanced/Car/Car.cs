408af00aadff098288d6736b5a4fa95413bca871
Emanuel Shavepeyer <daru12378@gmail.com>
2022-10-06 17:00:20 +0300

    Problem solving

---
+++ SoftUniAdvancedProgramming/SoftuniAdvanced/Car/Car.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CarManufacturer
+{
+
+    public class Car
+    {
+
+        public string Make { get { return make; } set { make = value; } }
+        public string Model { get { return model; } set { model = value; } }
+        public int Year { get { return year; } set { year = value; } }
+
+        private string make;
+        private string model;
+        private int year;
+    }
+}
