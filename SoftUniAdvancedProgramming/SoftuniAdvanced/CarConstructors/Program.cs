278cf8beffcf63623f6fbded9c33c771bc14458b
Emanuel Shavepeyer <daru12378@gmail.com>
2022-10-06 18:04:29 +0300

    Problem solving

---
+++ SoftUniAdvancedProgramming/SoftuniAdvanced/CarConstructors/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CarManufacturer
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var make = Console.ReadLine();
+            var model = Console.ReadLine();
+            var year = int.Parse(Console.ReadLine());
+            var fuelQuantity = double.Parse(Console.ReadLine());
+            var fuelConsumption = double.Parse(Console.ReadLine());
+
+            var car = new Car();
+            var car1 = new Car(make, model, year);
+            var car2 = new Car(make, model, year, fuelQuantity, fuelConsumption);
+
+
+        }
+    }
+}
