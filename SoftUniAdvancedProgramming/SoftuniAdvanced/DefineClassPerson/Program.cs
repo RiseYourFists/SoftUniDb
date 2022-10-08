c71419864fbe07cf89fe54a53543bac5a929ae75
Emanuel Shavepeyer <daru12378@gmail.com>
2022-10-08 13:26:06 +0300

    Problem solving

---
+++ SoftUniAdvancedProgramming/SoftuniAdvanced/DefineClassPerson/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var person1 = new Person("Peter", 20);
+            var person2 = new Person("George", 18);
+            var person3 = new Person("Jose", 43);
+        }
+    }
+}
