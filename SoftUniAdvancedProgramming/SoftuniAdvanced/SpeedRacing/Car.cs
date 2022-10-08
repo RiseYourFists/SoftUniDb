2b378e7973bda32942d165137bf30d257c2794f6
Emanuel Shavepeyer <daru12378@gmail.com>
2022-10-08 17:01:01 +0300

    Problem solving

---
+++ SoftUniAdvancedProgramming/SoftuniAdvanced/SpeedRacing/Car.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DefiningClasses
+{
+    public class Car
+    {
+        public Car(string model, double fuelAmount, double fuelConsumptionPerKm)
+        {
+            Model = model;
+            FuelAmount = fuelAmount;
+            FuelConsumptionPerKm = fuelConsumptionPerKm;
+            TravelledDistance = 0;
+        }
+
+        public string Model { get; set; }
+        public double FuelAmount { get; set; }
+        public double FuelConsumptionPerKm { get; set; }
+        public double TravelledDistance { get; set; }
+
+        public void Drive(double distance)
+        {
+            var burnedFuel = distance * this.FuelConsumptionPerKm;
+            if(this.FuelAmount - burnedFuel < 0 )
+            {
+                Console.WriteLine("Insufficient fuel for the drive");
+                return;
+            }
+
+            this.FuelAmount -= burnedFuel;
+            this.TravelledDistance += distance;
+        }
+
+        public string GetStats()
+        {
+            var sb = new StringBuilder();
+
+            sb.Append($"{this.Model} {this.FuelAmount:F2} {this.TravelledDistance}");
+            return sb.ToString();
+        }
+    }
+
+}
---
+++ SoftUniAdvancedProgramming/SoftuniAdvanced/SpeedRacing/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var cars = new Dictionary<string, Car>();
+
+            var inputs = int.Parse(Console.ReadLine());
+            for (int i = 0; i < inputs; i++)
+            {
+                var input = Console.ReadLine().Split();
+                var model = input[0];
+                var fuelAmount = double.Parse(input[1]);
+                var fuelConsumption = double.Parse(input[2]);
+
+                if (!cars.ContainsKey(model))
+                {
+                    cars.Add(model, new Car(model, fuelAmount, fuelConsumption));
+                }
+            }
+
+            string driveInfo;
+            while ((driveInfo = Console.ReadLine()) != "End")
+            {
+                var tokens = driveInfo.Split();
+                var model = tokens[1];
+                var distance = double.Parse(tokens[2]);
+
+                if (cars.ContainsKey(model))
+                {
+                    cars[model].Drive(distance);
+                }
+            }
+
+            foreach (var item in cars)
+            {
+                Console.WriteLine(item.Value.GetStats());
+            }
+        }
+    }
+}
