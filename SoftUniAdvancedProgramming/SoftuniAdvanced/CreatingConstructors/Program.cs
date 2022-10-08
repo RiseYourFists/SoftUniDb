bfcc11a8a7eb343d16c6db2daf3a6e1f557e6b68
Emanuel Shavepeyer <daru12378@gmail.com>
2022-10-08 13:46:16 +0300

    Problem solving

---
+++ SoftUniAdvancedProgramming/SoftuniAdvanced/CreatingConstructors/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var person1 = new Person();
+            var person2 = new Person(12);
+            var person3 = new Person("Joe", 13);
+
+            Console.WriteLine($"{person1.Name} {person1.Age}");
+            Console.WriteLine($"{person2.Name} {person2.Age}");
+            Console.WriteLine($"{person3.Name} {person3.Age}");
+        }
+    }
+}
