9c46da587ed9a35d962e0d2c1a02c15d19949081
Emanuel Shavepeyer <daru12378@gmail.com>
2022-10-08 16:15:24 +0300

    Problem solving

---
+++ SoftUniAdvancedProgramming/SoftuniAdvanced/OpinionPoll/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var people = new List<Person>();
+            var inputs = int.Parse(Console.ReadLine());
+            for (int i = 0; i < inputs; i++)
+            {
+                var input = Console.ReadLine().Split(' ');
+                var name = input[0];
+                var age = int.Parse(input[1]);
+
+                people.Add(new Person(name, age));
+            }
+
+            var sorted = SortPeople(people, (x => x.Age > 30));
+            foreach (var item in sorted)
+            {
+                Console.WriteLine($"{item.Name} - {item.Age}");
+            }
+        }
+        
+        public static List<Person> SortPeople(List<Person> collection, Predicate<Person> predicate)
+        {
+            var newCollection = collection.Where(x => predicate(x));
+            return newCollection.OrderBy(x => x.Name).ToList();
+        }
+    }
+}
