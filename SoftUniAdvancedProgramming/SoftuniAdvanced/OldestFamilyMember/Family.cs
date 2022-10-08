414106a7a27f98dde4dd6e4e5f69559ef1cd27dc
Emanuel Shavepeyer <daru12378@gmail.com>
2022-10-08 14:49:19 +0300

    Problem solving

---
+++ SoftUniAdvancedProgramming/SoftuniAdvanced/OldestFamilyMember/Family.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DefiningClasses
+{
+    public class Family
+    {
+        private List<Person> members;
+
+        public Family()
+        {
+            members = new List<Person>();
+        }
+
+        public List<Person> Members { get { return members; } set { members = value; } }
+
+        public void AddMember(string name, int age)
+        {
+            this.members.Add(new Person(name, age));
+        }
+
+        public Person GetOldest()
+        {
+            Person max = new Person(null, -1);
+            foreach (var item in this.members)
+            {
+                if(item.Age > max.Age)
+                {
+                    max = item;
+                }
+            }
+            return max;
+        }
+    }
+}
---
+++ SoftUniAdvancedProgramming/SoftuniAdvanced/OldestFamilyMember/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var inputs = int.Parse(Console.ReadLine());
+            var family = new Family();
+
+            for (int i = 0; i < inputs; i++)
+            {
+                var input = Console.ReadLine().Split();
+                var name = input[0];
+                var age = int.Parse(input[1]);
+
+                family.AddMember(name, age);
+            }
+
+            var oldest = family.GetOldest();
+
+            if (oldest.Name == null || oldest.Age == -1) return;
+
+            Console.WriteLine($"{oldest.Name} {oldest.Age}");
+        }
+    }
+}
