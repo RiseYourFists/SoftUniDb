fdcf739ec8672742f34e72d14c170a77a342810d
Emanuel Shavepeyer <daru12378@gmail.com>
2022-10-11 15:05:14 +0300

    Full refactoring

--- SoftUniAdvancedProgramming/SoftuniAdvanced/CarSalesman/Car.cs
+++ SoftUniAdvancedProgramming/SoftuniAdvanced/CarSalesman/Car.cs
@@ -6,17 +6,9 @@ namespace DefiningClasses
 {
     public class Car
     {
-        public Car(string model, Engine engine, int weight, string color)
-        {
-            Model = model;
-            Engine = engine;
-            Weight = weight;
-            Color = color;
-        }
-
         public string Model { get; set; }
         public Engine Engine { get; set; }
-        public int Weight { get; set; }
+        public int? Weight { get; set; }
         public string Color { get; set; }
     }
 }
--- SoftUniAdvancedProgramming/SoftuniAdvanced/CarSalesman/Program.cs
+++ SoftUniAdvancedProgramming/SoftuniAdvanced/CarSalesman/Program.cs
@@ -9,8 +9,8 @@ namespace DefiningClasses
     {
         static void Main(string[] args)
         {
-            var engineInline = new Dictionary<string, Engine>();
-            var carList = new Dictionary<string, Car>();
+            var engineInline = new List<Engine>();
+            var carList = new List<Car>();
 
             int inputs = int.Parse(Console.ReadLine());
             GetEngines(engineInline, inputs);
@@ -21,104 +21,102 @@ namespace DefiningClasses
             Console.WriteLine(PrintCars(carList));
         }
 
-
-        private static void GetCars(Dictionary<string, Engine> engineInline, Dictionary<string, Car> carList, int inputs)
+        private static void GetCars(List<Engine> engineInline, List<Car> carList, int inputs)
         {
             for (int i = 0; i < inputs; i++)
             {
-                var newCar = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
-                var carParams = GetParams(newCar);
-                var model = carParams[0];
-                var enModel = carParams[1];
-                var weight = int.Parse(carParams[2]);
-                var color = carParams[3];
+                var input = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                var len = input.Length;
+                var model = input[0];
+                var enModel = input[1];
+                var engine = engineInline.FirstOrDefault(x => x.Model == enModel);
+                var car = new Car()
+                {
+                    Model = model,
+                    Engine = engine
+                };
 
-                carList.Add(model, new Car(model, engineInline[enModel], weight, color));
-            }
-        }
+                if(len == 4)
+                {
+                    car.Weight = int.Parse(input[2]);
+                    car.Color = input[3];
+                }
 
-        private static void GetEngines(Dictionary<string, Engine> engineInline, int inputs)
-        {
-            for (int i = 0; i < inputs; i++)
-            {
-                var newEngine = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
-                var engineParams = GetParams(newEngine);
-                var model = engineParams[0];
-                var power = int.Parse(engineParams[1]);
-                var displacement = int.Parse(engineParams[2]);
-                var efficiency = engineParams[3];
-
-                engineInline.Add(model, new Engine(model, power, displacement, efficiency));
-            }
-        }
-
-        private static List<string> GetParams(string[] data)
-        {
-            var len = data.Length;
-            var result = new List<string>();
-            switch (len)
-            {
-                case 2:
-                    result = data.ToList();
-                    result.Add("-1");
-                    result.Add("-1");
-                    break;
-                case 3:
-                    result.Add(data[0]);
-                    result.Add(data[1]);
-                    if (int.TryParse(data[2], out int val))
+                if(len == 3)
+                {
+                    if (int.TryParse(input[2], out var weight))
                     {
-                        result.Add(data[2]);
-                        result.Add("-1");
-                        break;
+                        car.Weight = weight;
                     }
-                    result.Add("-1");
-                    result.Add(data[2]);
-                    break;
-                default:
-                    result = data.ToList();
-                    break;
+                    else
+                    {
+                        car.Color = input[2];
+                    }
+                }
+                carList.Add(car);
             }
-
-            return result;
         }
 
-        public static bool IsNull(string input)
+        private static void GetEngines(List<Engine> engineInline, int inputs)
         {
-            if (int.TryParse(input, out int output))
+            for (int i = 0; i < inputs; i++)
             {
-                return output == -1;
-            }
+                var input = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                var len = input.Length;
+                var model = input[0];
+                var power = int.Parse(input[1]);
+                var engine = new Engine()
+                {
+                    Model = model,
+                    Power = power
+                };
 
-            return false;
+                if (len == 4)
+                {
+                    engine.Displacement = int.Parse(input[2]);
+                    engine.Efficiency = input[3];
+                }
+
+                if (len == 3)
+                {
+                    if (int.TryParse(input[2], out var displacement))
+                    {
+                        engine.Displacement = displacement;
+                    }
+                    else
+                    {
+                        engine.Efficiency = input[2];
+                    }
+                }
+                engineInline.Add(engine);
+            }
         }
-        public static bool IsNull(int input)
+
+        private static string IsNull(string input)
         {
-            return input == -1;
+            string notAvaliable = "n/a";
+            return input == null ? notAvaliable : input;
         }
-        private static string PrintCars(Dictionary<string, Car> carList)
+        
+        private static string IsNull(int? input)
+        {
+            string notAvaliable = "n/a";
+            return input == null ? notAvaliable : $"{input}";
+        }
+
+        private static string PrintCars(List<Car> carList)
         {
             var sb = new StringBuilder();
-            var notAvaliable = "n/a";
 
-            foreach (var item in carList.Values)
+            foreach (var car in carList)
             {
-                sb.AppendLine($"{item.Model}:");
-                sb.AppendLine($"{item.Engine.Model}:");
-                sb.AppendLine($"  Power: {item.Engine.Power}");
-
-                string displacement = (IsNull(item.Engine.Displacement)) ? notAvaliable : $"{item.Engine.Displacement}";
-                sb.AppendLine($"  Displacement: {displacement}");
-
-                string efficiency = (IsNull(item.Engine.Efficiency)) ? notAvaliable : $"{item.Engine.Efficiency}";
-                sb.AppendLine($"  Efficiency: {efficiency}");
-
-                string weight = (IsNull(item.Weight)) ? notAvaliable : $"{item.Weight}";
-                sb.AppendLine($"Weight: {weight}");
-
-                string color = (IsNull(item.Color)) ? notAvaliable : $"{item.Color}";
-                sb.AppendLine($"Color: {color}");
-
+                sb.AppendLine($"{car.Model}:");
+                sb.AppendLine($" {car.Engine.Model}:");
+                sb.AppendLine($"   Power: {car.Engine.Power}");
+                sb.AppendLine($"   Displacement: {IsNull(car.Engine.Displacement)}");
+                sb.AppendLine($"   Efficiency: {IsNull(car.Engine.Efficiency)}");
+                sb.AppendLine($" Weight: {IsNull(car.Weight)}");
+                sb = sb.AppendLine($" Color: {IsNull(car.Color)}");
             }
             return sb.ToString();
         }
