e9886cdc2de7afbd5fe7a67c4a12333f0c8f5568
Emanuel Shavepeyer <daru12378@gmail.com>
2022-10-11 16:07:00 +0300

    Problem solving

---
+++ SoftUniAdvancedProgramming/SoftuniAdvanced/PokemonTrainer/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var trainers = new Dictionary<string, Trainer>();
+            GetTrainers(trainers);
+
+            string input;
+            while ((input = Console.ReadLine()) != "End")
+            {
+                foreach (var item in trainers)
+                {
+                    item.Value.CheckElements(input);
+                }
+            }
+
+            var result = trainers.OrderByDescending(x => x.Value.Badges).ToList();
+            result.ForEach(x => Console.WriteLine($"{x.Key} {x.Value.Badges} {x.Value.Pokemons.Count}"));
+
+        }
+
+        private static void GetTrainers(Dictionary<string, Trainer> trainers)
+        {
+            string input;
+            while ((input = Console.ReadLine()) != "Tournament")
+            {
+                var tokens = input.Split();
+                var trainer = tokens[0];
+                var pokemon = tokens[1];
+                var element = tokens[2];
+                var health = int.Parse(tokens[3]);
+
+                if (!trainers.ContainsKey(trainer))
+                {
+                    trainers.Add(trainer, new Trainer(trainer));
+                }
+
+                trainers[trainer].AddPokemon(new Pokemon(pokemon, element, health));
+            }
+        }
+    }
+}
---
+++ SoftUniAdvancedProgramming/SoftuniAdvanced/PokemonTrainer/Trainer.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DefiningClasses
+{
+    public class Trainer
+    {
+        public Trainer(string name)
+        {
+            Name = name;
+            Badges = 0;
+            Pokemons = new List<Pokemon>();
+        }
+
+        public string Name { get; set; }
+        public int Badges { get; set; }
+        public List<Pokemon> Pokemons { get; set; }
+
+        public void AddPokemon(Pokemon pokemon)
+        {
+            this.Pokemons.Add(pokemon);
+        }
+        public void CheckElements(string element)
+        {
+            if (this.Pokemons.Any(x => x.Element == element))
+            {
+                Badges++;
+                return;
+            }
+
+            this.Pokemons.ForEach(x => x.Health -= 10);
+            CheckHealth();
+        }
+
+        private void CheckHealth()
+        {
+            var newPokemonList = new List<Pokemon>(this.Pokemons);
+            foreach (var item in this.Pokemons)
+            {
+                if (item.Health <= 0)
+                {
+                    newPokemonList.Remove(item);
+                }
+            }
+            this.Pokemons = new List<Pokemon>(newPokemonList);
+        }
+    }
+}
+
