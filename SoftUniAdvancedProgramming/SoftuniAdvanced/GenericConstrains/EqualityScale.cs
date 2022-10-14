c4dee1f4e72574084867d3177a995d0bed3fbe4e
Emanuel Shavepeyer <daru12378@gmail.com>
2022-10-14 20:55:18 +0300

    Problem solving

---
+++ SoftUniAdvancedProgramming/SoftuniAdvanced/GenericConstrains/EqualityScale.cs
@@ -0,0 +1,23 @@
+﻿namespace GenericScale
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class EqualityScale<T>
+    {
+        public EqualityScale(T left, T right)
+        {
+            this.left = left;
+            this.right = right;
+        }
+
+        public T left;
+        public T right;
+
+        public bool AreEqual()
+        {
+            return left.Equals(right);
+        }
+    }
+}
---
+++ SoftUniAdvancedProgramming/SoftuniAdvanced/GenericConstrains/StartUp.cs
@@ -0,0 +1,13 @@
+﻿namespace GenericScale
+{
+    using System;
+
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var equal1 = new EqualityScale<int>(5, 5);
+            Console.WriteLine(equal1.AreEqual());
+        }
+    }
+}
