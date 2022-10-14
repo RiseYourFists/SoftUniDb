aafa4d66a5e4b5275e8e38c01749c8e62320d31c
Emanuel Shavepeyer <daru12378@gmail.com>
2022-10-14 13:52:33 +0300

    Problem solving

---
+++ SoftUniAdvancedProgramming/SoftuniAdvanced/BoxOfT/Box.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BoxOfT
+{
+    public class Box<T>
+    {
+        private List<T> elements = new List<T>();
+        public int Count { get { return elements.Count; } }
+
+        public void Add(T element)
+        {
+            elements.Add(element);
+        }
+
+        public T Remove()
+        {
+            var item = elements[Count - 1];
+            elements.RemoveAt(Count - 1);
+            return item;
+        }
+    }
+}
