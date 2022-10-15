630f3f14f7f10e077d8aa0f96a8be5071c238510
Emanuel Shavepeyer <daru12378@gmail.com>
2022-10-15 22:11:07 +0300

    Problem solving

---
+++ SoftUniAdvancedProgramming/SoftuniAdvanced/GenericSwapMethod/Box.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericBoxOfString
+{
+    public class Box<T>
+    {
+        public Box()
+        {
+            list = new List<T>();
+        }
+
+        private List<T> list;
+
+        public void Add(T item)
+        {
+            list.Add(item);
+        }
+
+        public void Swap(int item1, int item2)
+        {
+            var holder = list[item1];
+            list[item1] = list[item2];
+            list[item2] = holder;
+        }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+
+            foreach (var value in this.list)
+            {
+                sb.AppendLine($"{value.GetType()}: {value}");
+            }
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ SoftUniAdvancedProgramming/SoftuniAdvanced/GenericSwapMethod/Program.cs
@@ -0,0 +1,27 @@
+﻿using GenericBoxOfString;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GenericSwapMethod
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var count = int.Parse(Console.ReadLine());
+            var newBox = new Box<int>();
+
+            for (int i = 0; i < count; i++)
+            {
+                newBox.Add(int.Parse(Console.ReadLine()));
+            }
+
+            var tokens = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            newBox.Swap(tokens[0], tokens[1]);
+
+            Console.WriteLine(newBox.ToString());
+        }
+    }
+}
