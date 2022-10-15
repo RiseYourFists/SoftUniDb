1196fbb1a1472a8c7faca425bf15e4df9c90ddc1
Emanuel Shavepeyer <daru12378@gmail.com>
2022-10-15 21:47:09 +0300

    Problem solving

---
+++ SoftUniAdvancedProgramming/SoftuniAdvanced/GenericBoxOfString/Box.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericBoxOfString
+{
+    public class Box<T>
+    {
+        public Box(T value)
+        {
+            this.value = value;
+        }
+        public T value;
+
+        public string ToString()
+        {
+            return $"{value.GetType()}: {value}";
+        }
+    }
+}
---
+++ SoftUniAdvancedProgramming/SoftuniAdvanced/GenericBoxOfString/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace GenericBoxOfString
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var size = int.Parse(Console.ReadLine());
+           var array = new Box<int>[size];
+
+            for (int i = 0; i < size; i++)
+            {
+                var num = int.Parse(Console.ReadLine());
+                array[i] = new Box<int>(num);
+            }
+
+            foreach (var item in array)
+            {
+                Console.WriteLine(item.ToString());
+            }
+        }
+    }
+}
