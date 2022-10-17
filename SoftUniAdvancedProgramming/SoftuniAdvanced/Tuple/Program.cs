26e057e63a0586fcdc46b93f1d6d67efadeb0cec
Emanuel Shavepeyer <daru12378@gmail.com>
2022-10-18 01:42:49 +0300

    Problem solving

---
+++ SoftUniAdvancedProgramming/SoftuniAdvanced/Tuple/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Immutable;
+using System.Linq;
+
+namespace Tuple
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var personInfo = Console.ReadLine().Split();
+            var name = personInfo.Take(2).Aggregate((x, y) => $"{x} {y}");
+            var adress = personInfo[2];
+            var city = personInfo.Last();
+
+            var beerInfo = Console.ReadLine().Split();
+            var drunk = beerInfo.Last() == "drunk";
+
+            var bankInfo = Console.ReadLine().Split();
+
+            var person = new MyTuple<string, string, string>(name, adress, city);
+            var beer = new MyTuple<string, int, bool>(beerInfo[0], int.Parse(beerInfo[1]), drunk);
+            var num = new MyTuple<string, double, string>(bankInfo[0], double.Parse(bankInfo[1]), bankInfo[2]);
+
+            Console.WriteLine(person.ToString());
+            Console.WriteLine(beer.ToString());
+            Console.WriteLine(num.ToString());
+        }
+    }
+}
