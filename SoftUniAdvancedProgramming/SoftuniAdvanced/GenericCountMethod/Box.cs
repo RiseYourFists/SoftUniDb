635a9e4a775d9eb4f0657dffd67aefbfea0fb28e
Emanuel Shavepeyer <daru12378@gmail.com>
2022-10-17 21:51:38 +0300

    Problem solving

---
+++ SoftUniAdvancedProgramming/SoftuniAdvanced/GenericCountMethod/Box.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GenericBoxOfString
+{
+    public class Box<T>
+        where T : IComparable
+    {
+        public Box()
+        {
+            Items = new List<T>();
+        }
+
+        private List<T> Items { get; set; }
+
+        public void Add(T item)
+        {
+            Items.Add(item);
+        }
+
+        public void Swap(int item1, int item2)
+        {
+            var holder = Items[item1];
+            Items[item1] = Items[item2];
+            Items[item2] = holder;
+        }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+
+            foreach (var value in this.Items)
+            {
+                sb.AppendLine($"{value.GetType()}: {value}");
+            }
+
+            return sb.ToString();
+        }
+
+        public int CompareCount(T itemComparrison)
+            => Items.Count(x => x.CompareTo(itemComparrison) > 0);
+    }
+}
---
+++ SoftUniAdvancedProgramming/SoftuniAdvanced/GenericCountMethod/Program.cs
@@ -0,0 +1,22 @@
+﻿using GenericBoxOfString;
+using System;
+
+namespace GenericCountMethod
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var newBox = new Box<string>();
+
+            var inputs = int.Parse(Console.ReadLine());
+            for (int i = 0; i < inputs; i++)
+            {
+                var input = Console.ReadLine();
+                newBox.Add(input);
+            }
+
+            Console.WriteLine(newBox.CompareCount(Console.ReadLine()));
+        }
+    }
+}
