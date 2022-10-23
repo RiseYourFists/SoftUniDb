5bb1d1f589c0596d5b62887a018a2f53a1de4332
Emanuel Shavepeyer <daru12378@gmail.com>
2022-10-23 20:41:30 +0300

    Not tested code

---
+++ SoftUniAdvancedProgramming/SoftuniAdvanced/ListyIterator/ListyIterator.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IteratosAndComparators
+{
+    public class ListyIterator<T> : IEnumerator<T>
+    {
+        public ListyIterator(T[] collection)
+        {
+            Reset();
+            Collection = new List<T>(collection);
+        }
+
+        private List<T> Collection { get; set; }
+
+        public T Current => Collection[index];
+
+        private int index;
+
+        public bool Move() => MoveNext();
+
+        public bool HasNext() => index + 1 < Collection.Count;
+
+        public void Print() 
+        {
+            var msg = (Collection.Count > 0 && index < Collection.Count) ? $"{Collection[index]}" : "Invalid Operation!";
+            Console.WriteLine(msg);
+        }
+
+        public bool MoveNext() => index++ < Collection.Count;
+
+        public void Reset()=> index = 0;
+
+        public void Dispose() { }
+
+        object IEnumerator.Current => Current;
+    }
+}
---
+++ SoftUniAdvancedProgramming/SoftuniAdvanced/ListyIterator/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace IteratosAndComparators
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split(' ');
+            var command = input[0];
+            var collection = new ListyIterator<string>(input.Skip(1).ToArray());
+
+            while (command != "END")
+            {
+                switch (command)
+                {
+                    case "HasNext":
+                        Console.WriteLine(collection.HasNext());
+                        break;
+                    case "Print":
+                        collection.Print();
+                        break ;
+                    case "Move":
+                        Console.WriteLine(collection.Move());
+                        break;
+                }
+
+                command = Console.ReadLine();
+            }
+        }
+    }
+}
