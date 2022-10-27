860869ea6805a59da03acc9c20f86b49d1e9ca94
Emanuel Shavepeyer <daru12378@gmail.com>
2022-10-27 17:23:03 +0300

    Problem solving

---
+++ SoftUniAdvancedProgramming/SoftuniAdvanced/Froggy/Frog.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Froggy
+{
+    public class Frog<T> : IEnumerable<T>
+    {
+        public Frog(List<T> pond)
+        {
+            Pond = new List<T>(pond);
+        }
+
+        private List<T> Pond{ get; set; }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < Pond.Count; i++)
+            {
+                if ((i + 1) % 2 == 1)
+                {
+                    yield return Pond[i];
+                }
+            }
+
+            for (int i = Pond.Count - 1; i >= 0; i--)
+            {
+                if((i + 1) % 2 == 0)
+                {
+                    yield return Pond[i];
+                }
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+}
---
+++ SoftUniAdvancedProgramming/SoftuniAdvanced/Froggy/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace Froggy
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var rocks = Console.ReadLine().Split(", ").Select(int.Parse).ToList();
+            var frog = new Frog<int>(rocks);
+
+            var sb = new StringBuilder();
+
+            foreach (var rock in frog)
+            {
+                sb.Append($"{rock}, ");
+            }
+
+            sb.Remove(sb.Length - 2, 2);
+            Console.WriteLine(sb);
+        }
+    }
+}
