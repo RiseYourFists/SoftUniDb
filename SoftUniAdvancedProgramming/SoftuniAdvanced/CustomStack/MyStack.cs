8aba893f179cead13688edeadaed2097fec27c97
Emanuel Shavepeyer <daru12378@gmail.com>
2022-10-27 16:17:39 +0300

    Problem solving

---
+++ SoftUniAdvancedProgramming/SoftuniAdvanced/CustomStack/MyStack.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CustomStack
+{
+    public class MyStack<T> : IEnumerable<T>
+    {
+        public MyStack()
+        {
+            stack = new List<T>();
+        }
+
+        private readonly List<T> stack;
+
+        private int Count { get { return stack.Count; } }
+
+        public void Push(T element)
+        {
+            stack.Add(element);
+        }
+
+        public T Pop()
+        {
+            if (Count > 0)
+            {
+                T element = stack[^1];
+                stack.Remove(element);
+                return element;
+            }
+
+            throw new InvalidOperationException("No elements");
+        }
+
+        public IEnumerator<T> GetEnumerator() => new MyStackEnumerator(stack);
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+        private class MyStackEnumerator : IEnumerator<T>
+        {
+
+            public MyStackEnumerator(List<T> collection)
+            {
+                stack = new List<T>(collection);
+                Reset();
+            }
+            private readonly List<T> stack;
+
+            public T Current => stack[index];
+
+            private int index;
+
+            object IEnumerator.Current => Current;
+
+            public void Dispose() { }
+
+            public bool MoveNext() => --index >= 0;
+
+            public void Reset() => index = stack.Count;
+        }
+
+    }
+}
---
+++ SoftUniAdvancedProgramming/SoftuniAdvanced/CustomStack/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+
+namespace CustomStack
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            var myStack = new MyStack<int>();
+
+            while ((input = Console.ReadLine()) != "END")
+            {
+                var tokens = input.Split(" ");
+                switch (tokens[0])
+                {
+                    case "Push":
+
+                        var items = string.Join("", tokens.Skip(1));
+                        var elements = items.Split(',', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+                        elements.ForEach(x => myStack.Push(x));
+                        break;
+
+                    case "Pop":
+
+                        try { myStack.Pop(); }
+                        catch (Exception ex) { Console.WriteLine(ex.Message); };
+                        break;
+                }
+            }
+
+            for (int i = 1; i <= 2; i++)
+            {
+                Print(myStack);
+            }
+        }
+
+        private static void Print(MyStack<int> myStack)
+        {
+            foreach (var item in myStack)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
