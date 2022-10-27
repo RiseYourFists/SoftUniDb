bf9c21fe9cd4710d9e6e9b18e7799e8c68baadbe
Emanuel Shavepeyer <daru12378@gmail.com>
2022-10-27 19:16:02 +0300

    Problem solving

---
+++ SoftUniAdvancedProgramming/SoftuniAdvanced/CustomComparator/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace CustomComparator
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var array = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            Func<int, int, int> customComparer = (x, y) =>
+            {
+                return (x % 2 == 0 && y % 2 != 0)
+                ? -1 : (x % 2 != 0 && y % 2 == 0)
+                ? 1 : x > y
+                ? 1 : x < y
+                ? -1 : 0; 
+            };
+
+            Array.Sort(array, (x,y) => customComparer(x,y));
+
+            Console.WriteLine(string.Join(' ', array));
+        }
+    }
+}
