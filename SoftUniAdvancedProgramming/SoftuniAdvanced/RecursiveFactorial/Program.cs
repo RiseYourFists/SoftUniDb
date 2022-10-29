fe6624287d43cf5648955f623eee9142e6d9f203
Emanuel Shavepeyer <daru12378@gmail.com>
2022-10-29 19:31:40 +0300

    Problem solving

---
+++ SoftUniAdvancedProgramming/SoftuniAdvanced/RecursiveFactorial/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace RecursiveFactorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var number = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(Factorial(number));
+        }
+
+        public static long Factorial(int number)
+        {
+            if (number == 0)
+            {
+                return 1;
+            }
+
+            return number * Factorial(number - 1);
+        }
+    }
+}
