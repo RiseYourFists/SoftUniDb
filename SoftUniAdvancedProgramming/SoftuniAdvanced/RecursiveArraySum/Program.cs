e67fb1f21a3df4fe4a8fc9b5bdf691122b62ee4b
Emanuel Shavepeyer <daru12378@gmail.com>
2022-10-29 19:20:50 +0300

    Problem solving

---
+++ SoftUniAdvancedProgramming/SoftuniAdvanced/RecursiveArraySum/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace RecursiveArraySum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var array = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            var result = ArraySum(array, 0);
+
+            Console.WriteLine(result);
+        }
+
+        public static int ArraySum(int[] array, int index)
+        {
+            if (index >= array.Length)
+            {
+                return 0;
+            }
+
+            return array[index] + ArraySum(array, index + 1);
+        }
+    }
+
+}
