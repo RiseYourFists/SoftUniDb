ccd03776a9951fdaaf3581b579952aa5d00e4477
Emanuel Shavepeyer <daru12378@gmail.com>
2023-02-06 21:24:36 +0200

    first commit

---
+++ DemoDb/DemoDb/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace DemoDb
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            using var connection = new SqlConnection(Config.connectionString);
+            connection.Open();
+
+            var qurey = @"SELECT TOP(5) 
+                             [FirstName]
+                            ,[LastName]
+                            ,[JobTitle]
+                            ,[Salary]
+                          FROM [Employees]";
+
+            var command = new SqlCommand(qurey, connection);
+            using var reader = command.ExecuteReader();
+            var counter = 1;
+            while (reader.Read())
+            {
+                string firstName = (string)reader["FirstName"];
+                string lastName = (string)reader["LastName"];
+                string jobTitle = (string)reader["JobTitle"];
+                decimal salary = (decimal)reader["Salary"];
+
+                Console.WriteLine($"#{counter}. {firstName} {lastName}:\n     -Job: {jobTitle}\n     -Payment: {salary:f2}$");
+                counter++;
+            }
+            reader.Close();
+
+            connection.Close();
+        }
+    }
+}
