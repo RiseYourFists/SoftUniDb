c6fc4cc52ae97468f9a7b52a1485f1439cca6d5e
Emanuel Shavepeyer <daru12378@gmail.com>
2023-02-10 17:18:24 +0200

    Code refactoring to solve future problems

---
+++ DemoDb/MinionNames/MinionNames.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinionNames
+{
+    public class MinionNames
+    {
+        public MinionNames(SqlConnection connection)
+        {
+            Connection = connection;
+        }
+
+        public SqlConnection Connection { get; }
+
+        public string GetMinionNamesOfVillain(int villainId, string queryFileName)
+        {
+            var queryReader = new QueryReader($"{queryFileName}");
+            queryReader.GetOrCreatePath();
+            queryReader.GetOrCreateFile();
+
+            var query = queryReader.ReadToEnd();
+            var command = new SqlCommand(query, Connection);
+
+            var parameter = new SqlParameter("@VillainId", villainId);
+            command.Parameters.Add(parameter);
+
+            var reader = command.ExecuteReader();
+            var sb = new StringBuilder();
+            var counter = 1;
+
+            if (!reader.HasRows)
+            {
+                sb.AppendLine($"No villain with ID {villainId} exists in the database.");
+            }
+
+            while (reader.Read())
+            {
+                string VillainName = (string)reader["VillainName"];
+
+                if (counter == 1)
+                {
+                    sb.AppendLine($"Villain: {VillainName}");
+                }
+
+                string minionName = (string)reader["MinionName"];
+
+                if (counter == 1 && string.IsNullOrWhiteSpace(minionName))
+                {
+                    sb.AppendLine("(no minions)");
+                    break;
+                }
+
+                int minionAge = (int)reader["MinionAge"];
+                sb.AppendLine($"{counter}. {minionName} {minionAge}");
+                counter++;
+            }
+
+            return sb.ToString();
+        }
+
+
+    }
+}
