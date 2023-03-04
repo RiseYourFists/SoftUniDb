cb2cbe05ed012aca6e70cace4a91f3bb78045193
Emanuel Shavepeyer <daru12378@gmail.com>
2023-03-04 02:31:38 +0200

    Problem solving

---
+++ DemoDb/ProductShop/DTOs/Import/ImportProductDto.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace ProductShop.DTOs.Import
+{
+    [JsonObject]
+    public class ImportProductDto
+    {
+        [JsonProperty("Name")]
+        [Required]
+        [MinLength(3)]
+        public string Name { get; set; } = null!;
+
+        [JsonProperty("Price")]
+        [Required]
+        public decimal Price { get; set; }
+
+        [JsonProperty("SellerId")]
+        [Required]
+        public int SellerId { get; set; }
+
+        [JsonProperty("BuyerId")]
+        public int? BuyerId { get; set; }
+    }
+}
---
+++ DemoDb/ProductShop/StartUp.cs
@@ -0,0 +1,86 @@
+﻿using System.ComponentModel.DataAnnotations;
+using AutoMapper;
+using Newtonsoft.Json;
+using ProductShop.Data;
+using ProductShop.DTOs.Import;
+using ProductShop.Models;
+
+namespace ProductShop
+{
+    public class StartUp
+    {
+        private static string directory;
+        private static IMapper mapper;
+        public static void Main()
+        {
+            var context = new ProductShopContext();
+            //directory = InitializeDirectory("users.json");
+            directory = InitializeDirectory("products.json");
+           
+            var json = File.ReadAllText(directory);
+
+            //context.Database.EnsureDeleted();
+            //context.Database.EnsureCreated();
+
+            var result = ImportProducts(context, json);
+            Console.WriteLine(result);
+        }
+
+        public static string ImportUsers(ProductShopContext context, string inputJson)
+        {
+            mapper = new Mapper(new MapperConfiguration(cfg
+                => cfg.AddProfile(typeof(ProductShopProfile))));
+
+            var jsonUsersData = JsonConvert.DeserializeObject<ImportUserDto[]>(inputJson);
+
+            var users = new List<User>();
+            foreach (var uDto in jsonUsersData)
+            {
+                var user = mapper.Map<User>(uDto);
+                users.Add(user);
+            }
+
+            context.AddRange(users);
+            context.SaveChanges();
+
+            return $"Successfully imported {users.Count}";
+        }
+
+        public static string ImportProducts(ProductShopContext context, string inputJson)
+        {
+            mapper = new Mapper(new MapperConfiguration(cfg 
+                => cfg.AddProfile(typeof(ProductShopProfile))));
+
+            var productsJsonData = JsonConvert
+                .DeserializeObject<ImportProductDto[]>(inputJson);
+
+            var products = new List<Product>();
+            foreach (var pDto in productsJsonData)
+            {
+                if (!IsValid(pDto))
+                {
+                    continue;
+                }
+                var product = mapper.Map<Product>(pDto);
+                products.Add(product);
+            }
+
+            context.AddRange(products);
+            context.SaveChanges();
+            return $"Successfully imported {products.Count}";
+        }
+
+        public static string InitializeDirectory(string fileName)
+        => Path.Combine("../../../Datasets", fileName);
+
+        private static bool IsValid(object obj)
+        {
+            var validationContext = new System.ComponentModel.DataAnnotations.ValidationContext(obj);
+            var validationResult = new List<ValidationResult>();
+
+            bool isValid = Validator.TryValidateObject(obj, validationContext, validationResult, true);
+            return isValid;
+        }
+
+    }
+}
